d7b4003788114fae9e9f1b31dd20ee9dcb65fdcc
Daniel Sanchez <desgarcia2112@gmail.com>
2024-12-18 21:33:23 -0600

    Update WeirdAlgorithm java code

--- ICPC/src/WeirdAlgorithm.java
+++ ICPC/src/WeirdAlgorithm.java
@@ -1,23 +1,22 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class WeirdAlgorithm {
 
-    public static void suma(int n){
-        var x = 0;
+    public static void suma(double n){
         StringBuilder builder = new StringBuilder();
-        while(x != 1){
-            builder.append(n);
+        DecimalFormat df = new DecimalFormat("#.##");
+        while(n != 1){
+            builder.append(df.format(n));
             builder.append(" ");
-            if (n == 1){
-                x = 1;
-            }
-            if ((n%2) == 0){
-                n /= 2;
-
-            } else if ((n%2) != 0) {
-                n = (n*3)+1;
+            if ((n%2) == 0.0){
+                n = n/2;
+            } else {
+                n = (n*3) + 1;
             }
         }
+
+        builder.append(1);
         System.out.println(builder.toString().trim());
     }
 
@@ -31,5 +30,7 @@ public class WeirdAlgorithm {
         long tiempo = fin - inicio;
         System.out.println("Tiempo de ejecución: " + tiempo + " nanosegundos");
         System.out.println("Tiempo de ejecución: " + (tiempo / 1_000_000) + " milisegundos");
+
     }
+
 }
